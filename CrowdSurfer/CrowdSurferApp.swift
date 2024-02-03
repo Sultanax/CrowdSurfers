7205791d12a7012f84340fdd52eb57ce14bd9104
Sultana Yeasmin <yeasmins2005@gmail.com>
2024-02-03 18:07:03 -0500

    Initial Commit

---
+++ CrowdSurfer/CrowdSurferApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrowdSurferApp.swift
+//  CrowdSurfer
+//
+//  Created by Sultana Yeasmin on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CrowdSurferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
